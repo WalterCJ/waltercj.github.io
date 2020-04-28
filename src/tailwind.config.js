195fc1bcb5fa9221d2f214fa1ba3bb7564c607ac
WalterCJ <waltercavalcantejunior@gmail.com>
2020-04-28 11:46:16 -0300

    Initial commit

---
+++ src/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Niramit', 'Helvetica', 'Arial', 'sans-serif']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
